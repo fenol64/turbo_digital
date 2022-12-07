68687eab7658a3a30058c43c779a432d9efb77bb
Fernando Oliveira <fernandinho.nco@gmail.com>
2022-12-07 15:32:09 -0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
                     <meta name="theme-color" content="#212121" />
                     <meta name="msapplication-navbutton-color" content="#212121" />
                     {/* add favicon */}
-                    <link rel="icon" type="image/png" href="/assets/favicon.png" />
+                    <link rel="icon" type="image/png" href="https://static.wixstatic.com/media/40bd24_290ea0deb2b74deea4ac9ee1b9f7c1d6~mv2.png/v1/crop/x_239,y_136,w_263,h_241/fill/w_251,h_230,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/WhatsApp%20Image%202021-09-24%20at%2021_35_39.png" />
 
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;700&family=Rubik:wght@400;600&display=swap" rel="stylesheet" />
